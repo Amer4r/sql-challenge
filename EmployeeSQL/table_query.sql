5f2f26bd2271ad8aa466e3ef1a14f4402955bc19
Amer Banaweer <amerbnr10@gmail.com>
2024-03-05 19:46:46 -0500

    update

--- EmployeeSQL/table_query.sql
+++ EmployeeSQL/table_query.sql
@@ -10,6 +10,14 @@ CREATE TABLE "titles" (
      )
 );
 
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
     "emp_title_id" VARCHAR(5)   NOT NULL,
@@ -23,25 +31,8 @@ CREATE TABLE "employees" (
      )
 );
 
-CREATE TABLE "dept_manager" (
-    "id" int   NOT NULL,
-    "dept_no" VARCHAR(4)   NOT NULL,
-    "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "id"
-     )
-);
-
-CREATE TABLE "departments" (
-    "dept_no" VARCHAR(4)   NOT NULL,
-    "dept_name" VARCHAR(20)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
 CREATE TABLE "dept_emp" (
-    "id" int   NOT NULL,
+    "id" SERIAL   NOT NULL,
     "emp_no" int   NOT NULL,
     "dept_no" VARCHAR(4)   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
@@ -50,7 +41,7 @@ CREATE TABLE "dept_emp" (
 );
 
 CREATE TABLE "salaries" (
-    "id" int   NOT NULL,
+    "id" SERIAL   NOT NULL,
     "emp_no" int   NOT NULL,
     "salary" int   NOT NULL,
     CONSTRAINT "pk_salaries" PRIMARY KEY (
@@ -58,14 +49,17 @@ CREATE TABLE "salaries" (
      )
 );
 
+CREATE TABLE "dept_manager" (
+    "id" SERIAL   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "id"
+     )
+);
+
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
@@ -74,5 +68,11 @@ ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no
 REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
